e10b8874587dc9af62d10500fe30c01769fa578f
Simon Veldhuijzen <simon.veldhuijzen@nedap.com>
2022-01-21 12:05:06 +0100

    remove need for s(p)eed

--- game/State.js
+++ game/State.js
@@ -1,18 +1,18 @@
-const random = require('random')
-const seedrandom = require('seedrandom')
+// const random = require("random")
+// const seedrandom = require('seedrandom')
 
 const BOARD_SIZE = 10
 const N_COLORS = 5
 class State {
     constructor(seed) {
-        random.use(seedrandom(seed))
+        // random.use(seedrandom(seed))
         this.seed = seed
         // this.board = new Array(BOARD_SIZE).map(() => new Array(BOARD_SIZE))
         this.board = []
         for (let i = 0; i < BOARD_SIZE; i++) {
             this.board.push([]);
             for (let j = 0; j < BOARD_SIZE; j++) {
-                this.board[i].push(random.int(0, N_COLORS))
+                this.board[i].push(Math.random.int(0, N_COLORS))
             }
         }
     }

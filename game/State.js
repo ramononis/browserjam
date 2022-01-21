1abae8fd415530cd7b0e9f0dab3ca5ca650cd367
Ramon Onis <ramon.onis@nedap.com>
2022-01-21 10:50:49 +0100

    goed begin

---
+++ game/State.js
@@ -0,0 +1,9 @@
+// const BOARD_SIZE = 10
+//
+// exports.State = class State {
+//     constructor() {
+//     }
+//     score1 = 0
+//     score2 = 0
+//     board = Array[BOARD_SIZE].map(() => Array[BOARD_SIZE])
+// }
---
+++ game/index.js
@@ -0,0 +1,27 @@
+const random = require('random')
+const seedrandom = require('seedrandom')
+const BOARD_SIZE = 10
+const N_COLORS = 5
+class State {
+    constructor(seed) {
+        random.use(seedrandom(seed))
+        this.seed = seed
+        // this.board = new Array(BOARD_SIZE).map(() => new Array(BOARD_SIZE))
+        this.board = []
+        for (let i = 0; i < BOARD_SIZE; i++) {
+            this.board.push([]);
+            for (let j = 0; j < BOARD_SIZE; j++) {
+                this.board[i].push(random.int(0, N_COLORS))
+            }
+        }
+    }
+
+    seed
+    score1 = 0
+    score2 = 0
+    board
+}
+
+let state = new State(41)
+
+console.log(state)

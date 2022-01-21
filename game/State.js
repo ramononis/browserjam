107fb9d4f850ae018db201eceb4f7279fbc440ea
Ramon Onis <ramon.onis@nedap.com>
2022-01-21 10:52:28 +0100

    toch maar niet alles in 1 file

--- game/State.js
+++ game/State.js
@@ -1,9 +1,25 @@
-// const BOARD_SIZE = 10
-//
-// exports.State = class State {
-//     constructor() {
-//     }
-//     score1 = 0
-//     score2 = 0
-//     board = Array[BOARD_SIZE].map(() => Array[BOARD_SIZE])
-// }
+const random = require('random')
+const seedrandom = require('seedrandom')
+
+const BOARD_SIZE = 10
+const N_COLORS = 5
+class State {
+    constructor(seed) {
+        random.use(seedrandom(seed))
+        this.seed = seed
+        // this.board = new Array(BOARD_SIZE).map(() => new Array(BOARD_SIZE))
+        this.board = []
+        for (let i = 0; i < BOARD_SIZE; i++) {
+            this.board.push([]);
+            for (let j = 0; j < BOARD_SIZE; j++) {
+                this.board[i].push(random.int(0, N_COLORS))
+            }
+        }
+    }
+
+    seed
+    score1 = 0
+    score2 = 0
+    board
+}
+exports.State = State
--- game/index.js
+++ game/index.js
@@ -1,26 +1,4 @@
-const random = require('random')
-const seedrandom = require('seedrandom')
-const BOARD_SIZE = 10
-const N_COLORS = 5
-class State {
-    constructor(seed) {
-        random.use(seedrandom(seed))
-        this.seed = seed
-        // this.board = new Array(BOARD_SIZE).map(() => new Array(BOARD_SIZE))
-        this.board = []
-        for (let i = 0; i < BOARD_SIZE; i++) {
-            this.board.push([]);
-            for (let j = 0; j < BOARD_SIZE; j++) {
-                this.board[i].push(random.int(0, N_COLORS))
-            }
-        }
-    }
-
-    seed
-    score1 = 0
-    score2 = 0
-    board
-}
+const {State} = require("./State");
 
 let state = new State(41)
 

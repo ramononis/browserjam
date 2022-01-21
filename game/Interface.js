da0550d7596f68b92610b9d2cf340d95504bfb7a
Simon Veldhuijzen <simon.veldhuijzen@nedap.com>
2022-01-21 13:01:31 +0100

    start of visualization

--- game/Interface.js
+++ game/Interface.js
@@ -1,6 +1,13 @@
 // TODO: index.html en visualize implementeren
 function visualize(state) {
-    document.body //...
+    const canvas = document.querySelector('#canvas');
+    console.log(canvas)
+    console.log("hallo")
+    const width = canvas.width;
+    const height = canvas.height;
+
+    let ctx = canvas.getContext('2d');
+    ctx.strokeRect(50, 50, 150, 100);
 }
 
-
+exports.visualize = visualize
--- game/index.js
+++ game/index.js
@@ -1,4 +1,12 @@
 const {State} = require("./State");
+const {visualize} = require("./Interface");
 
 let state = new State(1)
+
 console.log(state)
+
+document.addEventListener("DOMContentLoaded", () => { visualize({}) })
+
+    // document.onload(() => { visualize({}) })
+
+

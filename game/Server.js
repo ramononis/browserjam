224650ca00636e2fef10d2325fba4293bd485e0a
Simon Veldhuijzen <simon.veldhuijzen@nedap.com>
2022-01-21 14:21:58 +0100

    server

--- game/Server.js
+++ game/Server.js
@@ -1,12 +1,12 @@
 const GAME_NAME = "Cupido"
-
 const {State} = require("./State");
 const {move} = require("./Logic");
 const {visualize} = require("./Interface");
 const { io } = require("socket.io-client");
 const socket = io("https://browserjam-event-server.herokuapp.com/" + GAME_NAME);
-const players = {}
+const players = new Map()
 let currentState = {}
+let lines = ""
 
 function joinGame(roomName, playerName) {
     socket.emit('join', {
@@ -15,9 +15,15 @@ function joinGame(roomName, playerName) {
     });
 }
 
+function leaveGame() {
+    socket.emit('leave');
+    addLog("Left the game")
+}
+
 function doMoveAndUpdateState(cell1, cell2) {
-    let newState = move(currentState, cell1, cell2)
-    socket.emit('state', newState);
+    // let newState = move(currentState, cell1, cell2)
+    currentState.turn = currentState.turn + 1
+    socket.emit('state', currentState);
 }
 
 function myTurn() {
@@ -26,10 +32,11 @@ function myTurn() {
 
 socket.on('join', player => {
     players[player.id] = player;
+    addLog(JSON.stringify(player) + " joined the game")
 
-    if ( player.id !== socket.id && players.size === 2 ) {
+    if ( player.id !== socket.id && Object.keys(players).length === 2 ) {
         currentState = new State(41)
-        currentState.turn = players.keys[Math.floor(Math.random(0, players.length))]
+        currentState.turn = Object.keys(players)[Math.floor(Math.random(0, players.length))]
         socket.emit('state', currentState);
     }
 
@@ -38,11 +45,23 @@ socket.on('join', player => {
     }
 });
 
-socket.on('state', (state, player) => {
-    currentState = state
-    if (player.id === socket.id) {
-        // ignore, player last moved
-    } else {
-        visualize(state)
-    }
+socket.on('state', player => {
+    addLog(JSON.stringify(player) + " left the game")
 });
+
+socket.on('leave', (state, player) => {
+    currentState = state
+    addLog("New state: " + JSON.stringify(currentState))
+    visualize(state)
+});
+
+function addLog(message) {
+    lines = lines + "<br/><br/>" + message
+    document.querySelector('#debugp').textContent = lines
+    document.querySelector('#debugp').innerHTML = lines
+}
+
+exports.joinGame = joinGame
+exports.doMoveAndUpdateState = doMoveAndUpdateState
+exports.myTurn = myTurn
+exports.leaveGame = leaveGame
--- game/index.js
+++ game/index.js
@@ -1,11 +1,19 @@
 const {State} = require("./State");
 const {visualize} = require("./Interface");
+const {joinGame} = require("./Server");
+const {doMoveAndUpdateState} = require("./Server");
+const {leaveGame} = require("./Server");
 
 let state = new State(1)
 
 console.log(state)
 
-document.addEventListener("DOMContentLoaded", () => { visualize({}) })
+document.addEventListener("DOMContentLoaded", () => {
+    visualize({})
+    document.querySelector('#join').addEventListener("click", () => { joinGame("testRoomThree", "player" + Math.floor(Math.random() * 1000)) })
+    document.querySelector('#move').addEventListener("click", () => { doMoveAndUpdateState({}, {}) })
+    document.querySelector('#leave').addEventListener("click", () => { leaveGame() })
+})
 
     // document.onload(() => { visualize({}) })
 
